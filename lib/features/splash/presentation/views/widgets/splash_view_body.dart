d33dcadbaf2da0815f2fdf317bfdbdf50eb6dc8a
usha7assan <ahd031103@gmail.com>
2025-03-02 14:14:20 +0200

    finish splash screen

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,35 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:graduation_project/core/utils/app_color.dart';
 import 'package:graduation_project/core/utils/styles.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  _SplashViewBodyState createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  double _scale = 0.1;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        _scale = 1.0;
+      });
+    });
+
+    // Future.delayed(Duration(seconds: 2), () {
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => LoginPage()),
+    //   );
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(
-            image: AssetImage('assets/images/logo.jpg'),
-            height: 100,
-            width: 100,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'asarak',
-                style: Styles.interBold40.copyWith(
-                  color: AppColor.textWhiteColor,
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: 0.1, end: _scale),
+        duration: Duration(seconds: 1),
+        curve: Curves.easeOut,
+        builder: (context, double scale, child) {
+          return Transform.scale(
+            scale: scale,
+            child: child,
+          );
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.jpg',
+              height: 100,
+              width: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'asarak',
+                  style: Styles.interBold40.copyWith(
+                    color: AppColor.textWhiteColor,
+                  ),
                 ),
-              ),
-              Text(
-                '\n',
-                style: Styles.latoBold12,
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
