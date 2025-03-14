5ba1a1a4823f7a3567b6acd21c89f9e1ff722a98
usha7assan <ahd031103@gmail.com>
2025-03-14 23:52:49 +0200

    add app logo name widget in core

---
+++ lib/core/widgets/app_logo_name.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/core/utils/app_color.dart';
+import 'package:graduation_project/core/utils/styles.dart';
+
+class AppLogoName extends StatelessWidget {
+  final double logoSize;
+  final double fontSize;
+
+  const AppLogoName({
+    super.key,
+    required this.logoSize,
+    required this.fontSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/logo.jpg',
+          height: logoSize,
+          width: logoSize,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'asarak',
+              style: Styles.latoBold36.copyWith(
+                color: AppColor.textWhiteColor,
+                decoration: TextDecoration.none,
+                fontSize: fontSize,
+              ),
+            ),
+            Text(
+              '\n',
+              style: Styles.latoBold12.copyWith(
+                fontSize: 13,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
