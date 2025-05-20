188cc46c4e81d697ab3ff850c3b06b7e69040b04
usha7assan <ahd031103@gmail.com>
2025-05-20 19:54:12 +0300

    change files location

--- lib/features/authentication/presentation/views/widgets/signUp/sign_up_view_body.dart
+++ lib/features/authentication/presentation/views/widgets/signUp/sign_up_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_project/core/utils/app_color.dart';
 import 'package:graduation_project/core/utils/styles.dart';
-import 'package:graduation_project/features/authentication/presentation/functions/registration_functions/registration_functions.dart';
+import 'package:graduation_project/features/authentication/functions/registration_functions/registration_functions.dart';
 import 'package:graduation_project/core/widgets/custom_button.dart';
 import 'package:graduation_project/features/authentication/presentation/views/widgets/custom_widgets/google_button.dart';
 import 'package:graduation_project/features/authentication/presentation/views/widgets/signUp/already_have_account.dart';
