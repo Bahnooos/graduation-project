3187361c61dc637a3dc710d0e0cef0d3bf201630
Bahnas <bahnas114@gmail.com>
2025-06-18 08:16:29 +0300

    no thing

--- lib/features/live_tracking/presentation/views/widgets/live_tracking_view_body.dart
+++ lib/features/live_tracking/presentation/views/widgets/live_tracking_view_body.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:graduation_project/core/routes/routes.dart';
-import 'package:graduation_project/core/widgets/custom_button.dart';
-import 'package:graduation_project/features/live_tracking/presentation/managers/camera%20detection%20cubit/camera_detection_cubit.dart';
 import 'package:graduation_project/features/live_tracking/presentation/views/widgets/custom_cmera_preview.dart';
 
 class LiveTrackingViewBody extends StatefulWidget {
@@ -22,7 +18,7 @@ class _LiveTrackingViewBodyState extends State<LiveTrackingViewBody> {
           Expanded(child: CustomCameraPreview()),
           // CustomButton(
           //   onPressed: () {
-             
+
           //   },
           //   text: 'Stop Tracking',
           // ),
